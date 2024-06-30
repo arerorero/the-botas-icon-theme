e5aabf8ed91732cd7ffed51613b6d42a2e18bf62
arerorero <com.filippo@outlook.com>
2024-06-30 18:22:35 -0300

    new prettier config

--- src/alfabetic.js
+++ src/alfabetic.js
@@ -1,24 +1,24 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 function sortJsonAlfabetically(json) {
-  let array = []
-  let newJson = {}
+  let array = [];
+  let newJson = {};
   for (const [key] of Object.entries(json)) {
-    array.push(key)
+    array.push(key);
   }
 
-  array.sort()
+  array.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: "base" }));
 
   array.forEach((element) => {
-    newJson[element] = json[element]
-  })
+    newJson[element] = json[element];
+  });
 
-  return newJson
+  return newJson;
 }
 
 export async function alfabetically() {
-  let icons = await fs.readFile("./icons.json", "utf-8")
-  icons = JSON.parse(icons)
+  let icons = await fs.readFile("./icons.json", "utf-8");
+  icons = JSON.parse(icons);
 
   const array = [
     "fileExtensions",
@@ -26,12 +26,12 @@ export async function alfabetically() {
     "folderNames",
     "folderNamesExpanded",
     "fileNames",
-  ]
+  ];
   array.forEach((element) => {
-    let name = icons[element]
-    name = sortJsonAlfabetically(name)
-    icons[element] = name
-  })
+    let name = icons[element];
+    name = sortJsonAlfabetically(name);
+    icons[element] = name;
+  });
 
-  await fs.writeFile("./icons.json", JSON.stringify(icons, null, 2))
+  await fs.writeFile("./icons.json", JSON.stringify(icons, null, 2));
 }
--- src/createPaths.js
+++ src/createPaths.js
@@ -1,24 +1,24 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 export async function createPaths() {
-  let icons = await fs.readFile("./icons.json", "utf-8")
-  icons = JSON.parse(icons)
+  let icons = await fs.readFile("./icons.json", "utf-8");
+  icons = JSON.parse(icons);
 
-  let iconDefinitions = {}
+  let iconDefinitions = {};
 
-  const directories = await fs.readdir("./images")
+  const directories = await fs.readdir("./images");
   await Promise.all(
     directories.map(async (directory) => {
-      const files = await fs.readdir(`./images/${directory}`)
+      const files = await fs.readdir(`./images/${directory}`);
       files.forEach((file) => {
-        const name = `_${file.split(".")[0].toLowerCase()}`
-        const route = `./images/${directory}/${file}`
+        const name = `_${file.split(".")[0].toLowerCase()}`;
+        const route = `./images/${directory}/${file}`;
         iconDefinitions[name] = {
           iconPath: route,
-        }
-      })
+        };
+      });
     })
-  )
-  icons.iconDefinitions = iconDefinitions
-  await fs.writeFile("./icons.json", JSON.stringify(icons, null, 2))
+  );
+  icons.iconDefinitions = iconDefinitions;
+  await fs.writeFile("./icons.json", JSON.stringify(icons, null, 2));
 }
