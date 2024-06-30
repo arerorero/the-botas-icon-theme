3e96b4d5aa53aec0aeb4f89801379cb2dd4e0646
arerorero <com.filippo@outlook.com>
2024-06-30 18:21:22 -0300

    function to create test files for each icons

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-import { alfabetically } from "./src/alfabetic.js"
-import { createPaths } from "./src/createPaths.js"
+import { alfabetically } from "./src/alfabetic.js";
+import { createPaths } from "./src/createPaths.js";
+import { createTests } from "./src/createTests.js";
 
-await createPaths()
-await alfabetically()
+await createPaths();
+await alfabetically();
+await createTests();
