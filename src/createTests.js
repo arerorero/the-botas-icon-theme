e7ee37eb80e2d84cae4206413a8060da4ca96e0c
arerorero <com.filippo@outlook.com>
2024-07-05 17:47:01 -0300

    more-icons: prettier was wrong

--- src/createTests.js
+++ src/createTests.js
@@ -1,12 +1,12 @@
-import fs from "fs/promises"
+import fs from "fs/promises";
 
 async function createFolders(icons) {
-  const folderNames = icons.folderNames
+  const folderNames = icons.folderNames;
   for (const [key] of Object.entries(folderNames)) {
     try {
-      await fs.access(`./test/${key}`)
+      await fs.access(`./test/${key}`);
     } catch {
-      await fs.mkdir(`./test/${key}`)
+      await fs.mkdir(`./test/${key}`);
     }
   }
 }
@@ -14,19 +14,19 @@ async function createFolders(icons) {
 async function createFiles(file, fileName = "") {
   for (const [key] of Object.entries(file)) {
     try {
-      await fs.readFile(`./test/${fileName}${key}`, "utf-8")
+      await fs.readFile(`./test/${fileName}${key}`, "utf-8");
     } catch {
-      fs.writeFile(`./test/${fileName}${key}`, "")
+      fs.writeFile(`./test/${fileName}${key}`, "");
     }
   }
 }
 
 export async function createTests() {
-  await fs.rm("./test", { recursive: true, force: true })
-  await fs.mkdir("./test")
-  let icons = await fs.readFile("./icons.json", "utf-8")
-  icons = JSON.parse(icons)
-  await createFolders(icons)
-  await createFiles(icons.fileNames)
-  await createFiles(icons.fileExtensions, ".")
+  await fs.rm("./test", { recursive: true, force: true });
+  await fs.mkdir("./test");
+  let icons = await fs.readFile("./icons.json", "utf-8");
+  icons = JSON.parse(icons);
+  await createFolders(icons);
+  await createFiles(icons.fileNames);
+  await createFiles(icons.fileExtensions, ".");
 }
