746a5937f281bc67a32330e5e90ad0f06b3bb765
arerorero <com.filippo@outlook.com>
2024-07-05 17:43:18 -0300

    more-icons: Better organized the icons and remoduled the function that created their's paths in icons.json closes more-icons branch

--- src/createTests.js
+++ src/createTests.js
@@ -1,33 +1,32 @@
-import fs from "fs/promises";
+import fs from "fs/promises"
 
 async function createFolders(icons) {
-  const folderNames = icons.folderNames;
+  const folderNames = icons.folderNames
   for (const [key] of Object.entries(folderNames)) {
     try {
-      await fs.access(`./test/${key}`);
+      await fs.access(`./test/${key}`)
     } catch {
-      await fs.mkdir(`./test/${key}`);
+      await fs.mkdir(`./test/${key}`)
     }
   }
 }
 
 async function createFiles(file, fileName = "") {
   for (const [key] of Object.entries(file)) {
-    console.log(key);
     try {
-      await fs.readFile(`./test/${fileName}${key}`, "utf-8");
+      await fs.readFile(`./test/${fileName}${key}`, "utf-8")
     } catch {
-      fs.writeFile(`./test/${fileName}${key}`, "");
+      fs.writeFile(`./test/${fileName}${key}`, "")
     }
   }
 }
 
 export async function createTests() {
-  await fs.rm("./test", { recursive: true, force: true });
-  await fs.mkdir("./test");
-  let icons = await fs.readFile("./icons.json", "utf-8");
-  icons = JSON.parse(icons);
-  await createFolders(icons);
-  await createFiles(icons.fileNames);
-  await createFiles(icons.fileExtensions, ".");
+  await fs.rm("./test", { recursive: true, force: true })
+  await fs.mkdir("./test")
+  let icons = await fs.readFile("./icons.json", "utf-8")
+  icons = JSON.parse(icons)
+  await createFolders(icons)
+  await createFiles(icons.fileNames)
+  await createFiles(icons.fileExtensions, ".")
 }
